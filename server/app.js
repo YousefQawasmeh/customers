605d4b4eb238319ec4c8e48df7465fc7aec679ce
YousefQwasmeh <151037@ppu.edu.ps>
2021-02-15 14:07:00 +0200

    create server

---
+++ server/app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const axios = require("axios");
+
+app.get("/", (req, res) => {
+  res.send("0");
+});
+
+app.get("/api/:customerUrl", async (req, res) => {
+  const resp = await axios(
+    "https://you97sef.herokuapp.com/api/getRecordsUrl/" + req.params.customerUrl
+  ).catch((err) => {
+    res.redirect("/");
+    console.log("00000000001", err, "0000000001");
+  });
+  res.json(resp.data);
+});
+
+app.use((req, res) => {
+  res.redirect("/");
+});
+
+module.exports = app;
